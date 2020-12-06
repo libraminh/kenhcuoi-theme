f97245636b717c2e9f0af63c2d4973d46023eb16
minh le <minh.le@construct.sg>
2020-12-07 01:11:40 +0700

    update breadcrumbs

--- template-parts/global/breadcrumb.php
+++ template-parts/global/breadcrumb.php
@@ -1,17 +1,14 @@
 <?php 
-  add_filter( 'wpseo_breadcrumb_single_link', 'change_breadcrumb_link_class', 10, 2); 
   function change_breadcrumb_link_class($link_output, $link) {  
     $output .= '
       <li><a href="' . $link['url'] . '">' . $link['text'] . '</a></li>
     ';
     return $output;
-  }
-
+  };
   function filter_wpseo_breadcrumb_output( $output ) { 
     return str_replace(array("<span>","</span>"), "", $output);
   }; 
-          
-  // add the filter 
+  add_filter( 'wpseo_breadcrumb_single_link', 'change_breadcrumb_link_class', 10, 2); 
   add_filter( 'wpseo_breadcrumb_output', 'filter_wpseo_breadcrumb_output', 10, 1 ); 
 ?>
 
@@ -20,7 +17,7 @@
     <ol class="breadcrumb breadcrumb-1 text-uppercase">
       <?php
         if ( function_exists('yoast_breadcrumb') ) {
-          yoast_breadcrumb( '<ol id="breadcrumbs" class="breadcrumb breadcrumb-1 text-uppercase">','</ol>' );
+          yoast_breadcrumb();
         };
       ?>
     </ol>
