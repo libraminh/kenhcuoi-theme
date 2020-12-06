34791469f9fbd7b7b5fea8629e9211b214ac03e1
minh le <minh.le@construct.sg>
2020-12-06 13:38:51 +0700

    update masthead global

--- template-parts/global/masthead-global.php
+++ template-parts/global/masthead-global.php
@@ -1,9 +1,20 @@
+<?php 
+  $cate_slug = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+  $cate_name = get_category_by_slug($cate_slug)->name;
+?>
+
 <section class="masthead--global">
   <div class="home-carousel-wrap">
     <img class="lace-shadow" src="<?php echo get_template_directory_uri(); ?>/img/home/ren-shadow.png" alt="">
     <img class="lace-shadow" src="<?php echo get_template_directory_uri(); ?>/img/home/ren-shadow-bottom.png" alt="">
     <div class="home-carousel-wrap-carousel1">
       <img src="<?php echo get_template_directory_uri(); ?>/img/carousel-bg.png" alt="">
+
+      <?php  if (is_archive()) : ?>
+      <div class="container">
+        <h3 class="text-uppercase carousel-title"><?php echo $cate_name; ?></h3>
+      </div>
+      <?php endif; ?>
     </div>
   </div>
 </section>
