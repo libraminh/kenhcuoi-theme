c43d62ad64d8297ea78dde0a41d573ecbc428f8b
Minh Le <minh.le@construct.sg>
2020-12-09 12:13:31 +0700

    update home masthead

--- template-parts/home/home-masthead.php
+++ template-parts/home/home-masthead.php
@@ -3,15 +3,12 @@
     <img class="lace" src="<?php echo get_template_directory_uri(); ?>/img/home/ren4.svg" alt="">
     <img class="lace lace-bottom" src="<?php echo get_template_directory_uri(); ?>/img/home/ren.svg" alt="">
 
-    <?php $slider = get_field('slider_post'); ?>
-
-    <?php if( have_rows('masthead_slider', $slider->ID) ): ?>
+    <?php if( have_rows('masthead_slider') ): ?>
       <div class="owl-carousel owl-theme home-carousel-wrap-carousel">
-        <?php while( have_rows('masthead_slider', $slider->ID) ): the_row(); 
+        <?php while( have_rows('masthead_slider') ): the_row(); 
           $image = get_sub_field('image');
           $link = get_sub_field('link');
         ?>
-      
           <div class="item">
             <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 
@@ -21,7 +18,6 @@
               </a>
             </div>
           </div>
-
         <?php endwhile; ?>
       </div>
     <?php endif; ?>
--- template-parts/post/post-title.php
+++ template-parts/post/post-title.php
@@ -1,3 +1,4 @@
+<div class="margin-box-2"></div>
 <?php 
   if ( is_singular() ) :
     the_title( '<h4 class="entry-title home-showcase-title text-uppercase"><strong>', '</strong></h4>' );
@@ -5,3 +6,4 @@
     the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
   endif;
 ?>
+<div class="margin-box-2"></div>
