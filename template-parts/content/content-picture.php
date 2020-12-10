0b05b2b787a30cd34a611be4a8ec07c68b9c4d19
minh le <minh.le@construct.sg>
2020-12-10 22:06:01 +0700

    update content picture

--- template-parts/content/content-picture.php
+++ template-parts/content/content-picture.php
@@ -13,22 +13,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('promotion-detail'); ?>>
-	
+
   <?php get_template_part( 'template-parts/post/post', 'header' ); ?>
   <?php get_template_part( 'template-parts/post/post', 'gallery' ); ?>
   <?php get_template_part( 'template-parts/post/post', 'title' ); ?>
-  <?php get_template_part( 'template-parts/post/post', 'metadata' ); ?>
+  <?php get_template_part( 'template-parts/post/post', 'metadata-default' ); ?>
   <?php get_template_part( 'template-parts/post/post', 'more-link' ); ?>
   <?php get_template_part( 'template-parts/post/post', 'content' ); ?>
   <?php get_template_part( 'template-parts/post/post', 'share-bar'); ?>
   <?php get_template_part( 'template-parts/partials/post', 'ads'); ?>
-  
+
   <div class="margin-box"></div>
-  
+
   <?php get_template_part( 'template-parts/post/post', 'related', $args); ?>
   <?php get_template_part( 'template-parts/post/post', 'show-all'); ?>
 
-	<footer class="entry-footer">
-		<?php kenhcuoi_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+  <footer class="entry-footer">
+    <?php kenhcuoi_entry_footer(); ?>
+  </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
