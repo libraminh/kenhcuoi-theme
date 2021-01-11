59901df0ec95988de26c72e309379b3aa1059712
Minh Le <minh.le@construct.sg>
2021-01-11 17:14:56 +0700

    update css and price quote modal

---
+++ template-parts/partials/price-quote-modal.php
@@ -0,0 +1,15 @@
+<!-- Modal-->
+<div class="modal fade" id="service-detail-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  style="display: none;">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header"><img class="modal-lace" src="assets/img/ren.png" alt="">
+        <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span
+            aria-hidden="true">×</span></button>
+        <h4 class="modal-title text-uppercase text-accent text-center"><strong>Đăng ký nhận báo giá</strong></h4>
+      </div>
+
+      <?php echo do_shortcode('[contact-form-7 id="430" title="Price Quote Form"]') ?>
+    </div>
+  </div>
+</div>
