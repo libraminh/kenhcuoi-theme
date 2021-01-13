5bd5b474ae9512076a4ef11a537c45a439769711
Minh Le <minh.le@construct.sg>
2021-01-13 14:29:34 +0700

    update post more link

--- template-parts/post/post-more-link.php
+++ template-parts/post/post-more-link.php
@@ -1,17 +1,21 @@
+<?php
+  $perPage = 4;
+  $args = array(
+    'orderby'   => 'rand',
+    'post_type' => 'any',
+    'posts_per_page' => $perPage
+  );
+  $the_query = new WP_Query( $args ); 
+?>
+
+<?php if($the_query->have_posts()) : ?>
+
 <div class="post-video-showcase-morelink">
   <ul class="post-video-showcase-morelink-nav">
-    <?php 
-    $perPage = 4;
-    $args = array(
-      'orderby'   => 'rand',
-      'post_type' => 'any',
-      'posts_per_page' => $perPage
-    );
-    $the_query = new WP_Query( $args ); 
-    while ($the_query -> have_posts()) : $the_query -> the_post() ?>
+    <?php while ($the_query -> have_posts()) : $the_query -> the_post() ?>
     <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-    <?php endwhile;
-    wp_reset_postdata();
-    ?>
+    <?php endwhile; ?>
   </ul>
 </div>
+
+<?php endif; wp_reset_postdata(); ?>
