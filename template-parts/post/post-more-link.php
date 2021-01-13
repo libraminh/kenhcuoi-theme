4d64f410892a8f5481be66854340e1cc605bb03c
Minh Le <minh.le@construct.sg>
2021-01-13 14:18:12 +0700

    update post more link

--- template-parts/post/post-more-link.php
+++ template-parts/post/post-more-link.php
@@ -1,8 +1,17 @@
 <div class="post-video-showcase-morelink">
   <ul class="post-video-showcase-morelink-nav">
-    <li><a href="#">Gói chụp ảnh cưới chuyên nghiệp - tự nhiên - nghệ thuật</a></li>
-    <li><a href="#">Gói combo Wedding "Biển xanh vẫy gọi" siêu tiết kiệm chỉ với với 8.600.000 VNĐ</a></li>
-    <li><a href="#">Chụp hình cưới trọn gói giá rẻ chỉ 2.990.000đ – Tại phim trường Mini Xinh Xinh</a></li>
-    <li><a href="#">Dịch vụ chụp ảnh ăn hỏi đám cưới, chụp ảnh phóng sự cưới-MTP Studio</a></li>
+    <?php 
+    $perPage = 4;
+    $args = array(
+      'orderby'   => 'rand',
+      'post_type' => 'any',
+      'posts_per_page' => $perPage
+    );
+    $the_query = new WP_Query( $args ); 
+    while ($the_query -> have_posts()) : $the_query -> the_post() ?>
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile;
+    wp_reset_postdata();
+    ?>
   </ul>
 </div>
